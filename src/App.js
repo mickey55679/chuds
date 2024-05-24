71ce7ced3773fea5b35270f3e083fbeed5d2a575
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-24 15:14:54 -0500

    re-organized components into index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavigationBar from "./components/navigation";
-import Home from "./components/home";
-import Menu from "./components/menu"; 
-import Footer from "./components/footer";
-import ContactForm from "./components/contact";
-import Login from './components/Login'
+import {ContactForm, Footer, Menu, Home, Login, NavigationBar} from './components/index'
 
 
 function App() {
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import ContactForm from './ContactForm'
+import Footer from './Footer'
+import Home from './Home'
+import Login from './Login'
+import Menu from './Menu'
+import NavigationBar from './NavigationBar'
+import Signup from './Signup'
+
+
+export {ContactForm, Footer, Home, Login, Menu, NavigationBar, Signup};
