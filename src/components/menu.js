2ad5a2bbc74bbef015a20e008b9f63be445293ae
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-24 21:15:14 -0500

    updates to menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,15 +1,44 @@
 import React from "react";
 
+
 const Menu = () => {
+  const menuItems = [
+    {
+      href: "",
+      src: "https://cdn.pixabay.com/photo/2021/01/06/10/11/burger-5893927_1280.jpg",
+      alt: "",
+      desc: "Add a description of the image here",
+    },
+    {
+      href: "",
+      src: "",
+      alt: "",
+      desc: "Add a description of the image here",
+    },
+    {
+      href: "",
+      src: "",
+      alt: "",
+      desc: "Add a description of the image here",
+    },
+    {
+      href: "",
+      src: "",
+      alt: "",
+      desc: "Add a description of the image here",
+    },
+  ];
+
   return (
     <div>
-      <h2>Menu</h2>
-      <ul>
-        <li>Burgers</li>
-        <li>Wings</li>
-        <li>Appetizers</li>
-        {/* Add more menu items as needed */}
-      </ul>
+      {menuItems.map((item, index) => (
+        <div key={index} className="menu-items">
+          <a target="_blank" rel="noopener noreferrer" href={item.href}>
+            <img src={item.src} alt={item.alt} width="600" height="400" />
+          </a>
+          <div className="desc">{item.desc}</div>
+        </div>
+      ))}
     </div>
   );
 };
