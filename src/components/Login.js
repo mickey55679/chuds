94edc44ca2b396c7948e2c9a9a29eac76ff5aa54
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-26 16:19:48 -0500

    wip

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,49 +1,12 @@
 import React, { useState } from "react";
 
 const Login = () => {
-  // State for determining if the form is in login mode or sign up mode
   const [isLogin, setIsLogin] = useState(true);
-
-  // State for storing username input
   const [username, setUsername] = useState("");
-
-  // State for storing password input
   const [password, setPassword] = useState("");
-
-  // State for storing email input (only used in sign up mode)
   const [email, setEmail] = useState("");
-
-  // State for storing the "Remember me" checkbox value
   const [isRemembered, setIsRemembered] = useState(false);
 
-  // Style for centering the form in the middle of the page
-  const centerStyle = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "75vh",
-    flexDirection: "column",
-  };
-
-  // Style for the form
-  const formStyle = {
-    backgroundColor: "white",
-    padding: "16px",
-    borderRadius: "8px",
-    boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.1)",
-  };
-
-  // Style for the input fields
-  const inputStyle = {
-    width: "100%", // You can adjust this
-    padding: "12px 20px",
-    margin: "8px 0",
-    display: "inline-block",
-    border: "1px solid #ccc",
-    boxSizing: "border-box",
-  };
-
-  // Function to clear the form fields
   const clearForm = () => {
     setUsername("");
     setPassword("");
@@ -51,15 +14,14 @@ const Login = () => {
   };
 
   return (
-    <div style={centerStyle}>
-      <form style={formStyle} action="action_page.php" method="post">
+    <div className="center-style">
+      <form className="form-style" action="action_page.php" method="post">
         <h3>
           Don't have an account yet?{" "}
           <a href="/create-account">Create an account</a>
         </h3>
 
         <div className="imgcontainer">
-          {/* Display an avatar image */}
           <img
             src="https://scontent.ffsd2-1.fna.fbcdn.net/v/t39.30808-6/436415599_407791682035993_8168994351067320526_n.jpg?stp=cp6_dst-jpg&_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=C2ZS5NYEhEgQ7kNvgE-hSSS&_nc_ht=scontent.ffsd2-1.fna&oh=00_AYB8JZpx43fbwn7eK0Ek0XPsth_cFxI3_ZgzMnA1OrNoCg&oe=665708A9"
             alt="Avatar"
@@ -71,19 +33,18 @@ const Login = () => {
           <label htmlFor="uname">
             <b>Username</b>
           </label>
-          {/* Input field for username */}
           <input
-            style={inputStyle}
+            className="input-style"
             type="text"
             placeholder="Enter Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
           />
-          {/* Input field for email, displayed only in sign up mode */}
+
           {!isLogin && (
             <input
-              style={inputStyle}
+              className="input-style"
               type="email"
               placeholder="Enter Email"
               value={email}
@@ -92,9 +53,8 @@ const Login = () => {
             />
           )}
 
-          {/* Input field for password */}
           <input
-            style={inputStyle}
+            className="input-style"
             type="password"
             placeholder="Enter Password"
             value={password}
@@ -102,16 +62,15 @@ const Login = () => {
             required
           />
 
-          {/* Submit button, text changes based on the form mode */}
           <button type="submit" className="button-log">
             {isLogin ? "Login" : "Sign Up"}
           </button>
-          {/* Checkbox for "Remember me" */}
+
           <label>
             <input
               type="checkbox"
-              checked={isRemembered} // Use the state variable here
-              onChange={() => setIsRemembered(!isRemembered)} // Update the state when the checkbox is clicked
+              checked={isRemembered}
+              onChange={() => setIsRemembered(!isRemembered)}
               name="remember"
             />
             Remember me
@@ -119,14 +78,12 @@ const Login = () => {
         </div>
 
         <div className="container" style={{ backgroundColor: "#f1f1f1" }}>
-          {/* Cancel button to clear the form */}
           <button type="button" className="button-log" onClick={clearForm}>
             Cancel
           </button>
           <span className="psw">
             Forgot <a href="#">password?</a>
           </span>
-          {/* Button to switch between login and sign up modes */}
           <button
             type="button"
             className="button-log"
