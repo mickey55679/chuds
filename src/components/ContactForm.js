b7da272a046031522620349189cf2e7589f48c10
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-26 15:37:54 -0500

    wip notes added

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
 
 const ContactForm = () => {
+  // State for form data
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
+
+  // State for loading status
   const [isLoading, setIsLoading] = useState(false);
+
+  // State for form submission message
   const [formMessage, setFormMessage] = useState("");
 
+  // Handle input changes
   const handleChange = (e) => {
+    // Update formData state with new input values
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  // Handle form submission
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+    e.preventDefault(); // Prevent default form submission behavior
+    setIsLoading(true); // Set loading state to true
     setFormMessage(""); // Clear previous form message
 
+    // Send POST request to the backend
     fetch(process.env.REACT_APP_BACKEND_URL, {
       method: "POST",
       headers: {
@@ -30,17 +39,22 @@ const ContactForm = () => {
       }),
     })
       .then((response) => {
+        // Check if response is ok (status code 2xx)
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
+        // Parse JSON response
         return response.json();
       })
       .then((data) => {
+        // Set form message from server response
         setFormMessage(data.message);
         console.log(data);
-        setFormData({ name: "", email: "", message: "" }); // Reset form
+        // Reset form fields
+        setFormData({ name: "", email: "", message: "" });
       })
       .catch((error) => {
+        // Handle errors
         console.error("Error:", error);
         setFormMessage("Something went wrong. Please try again later.");
       })
@@ -81,10 +95,12 @@ const ContactForm = () => {
           required
         />
       </div>
+      {/* Submit button, disabled when loading */}
       <button type="submit" className="button-27" disabled={isLoading}>
         {isLoading ? "Sending..." : "Submit"}
       </button>
-      {formMessage && <p>{formMessage}</p>} {/* Display form message */}
+      {/* Display form message */}
+      {formMessage && <p>{formMessage}</p>}
     </form>
   );
 };
