4c81b76f5b759413097a718b648058109fed2397
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-27 22:31:25 -0500

    wip

--- data/migrations/20240528025321_burgers.js
+++ data/migrations/20240528025321_burgers.js
@@ -3,12 +3,29 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable("burgers", function (table) {
-    table.increments("id").primary();
-    table.string("burger_name").notNullable();
-    table.decimal("price").notNullable();
-    table.string("imgurl").notNullable();
-  });
+  return knex.schema
+    .createTable("burgers", function (table) {
+      table.increments("id").primary();
+      table.string("burger_name").notNullable();
+      table.decimal("price").notNullable();
+      table.string("imgurl").notNullable();
+    })
+    .then(function () {
+      return knex.schema.createTable("sandwiches", function (table) {
+        table.increments("id").primary();
+        table.string("sandwich_name").notNullable();
+        table.decimal("price").notNullable();
+        table.string("imgurl").notNullable();
+      });
+    })
+    .then(function () {
+      return knex.schema.createTable("sides", function (table) {
+        table.increments("id").primary();
+        table.string("side_name").notNullable();
+        table.decimal("price").notNullable();
+        table.string("imgurl").notNullable();
+      });
+    });
 };
 
 /**
@@ -16,5 +33,12 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("burgers");
+  return knex.schema
+    .dropTableIfExists("sides")
+    .then(function () {
+      return knex.schema.dropTableIfExists("sandwiches");
+    })
+    .then(function () {
+      return knex.schema.dropTableIfExists("burgers");
+    });
 };
