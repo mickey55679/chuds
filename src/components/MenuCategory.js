87e17b0127330b389c604654b9ecb0eee8278280
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-01 11:59:34 -0500

    wip

---
+++ src/components/MenuCategory.js
@@ -0,0 +1,46 @@
+// MenuCategory.jsx
+import React from "react";
+
+const MenuCategory = ({ title, items, updateOrder, orderItems }) => {
+  return (
+    <div className="menu-container">
+      <h2>{title}</h2>
+      <div className="menu-category">
+        {items.map((item, index) => (
+          <div key={index} className="menu-item menu-items">
+            <h2>{item.name}</h2>
+            <img
+              src={item.imgurl}
+              alt={`${item.name} served on a plate`}
+              width="180"
+              height="auto"
+            />
+            <p>{item.description}</p>
+            <p>Category: {item.category}</p>
+            <p>Price: ${item.price.toFixed(2)}</p>
+            <div className="desc">{item.desc}</div>
+            <div className="menu-item-controls">
+              <button
+                className="update_order_subtract"
+                onClick={() => updateOrder(item.id, "-")}
+              >
+                -
+              </button>
+              <span className="item-quantity">
+                {orderItems[item.id] || "0"}
+              </span>
+              <button
+                className="update_order_add"
+                onClick={() => updateOrder(item.id, "+")}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MenuCategory;
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,184 +1,52 @@
 import React, { useEffect, useState } from "react";
+import MenuCategory from "./MenuCategory";
 
-const Menu = ({setCartItems, setItems}) => {
+const Menu = ({ setCartItems, setItems }) => {
   const [menuItems, setMenuItems] = useState({});
   const [orderItems, setOrderItems] = useState({});
 
   useEffect(() => {
     // Fetch the data from the server
     fetch("http://localhost:3000/menu")
-      .then((response) => {
-        // Parse the response as JSON
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
-        // Update the state with the fetched data
         setMenuItems(data);
-        setItems(data)
+        setItems(data);
       })
       .catch((error) => {
-        // Handle any errors that occur during the fetch
         console.error("Error:", error);
       });
   }, []); // Empty dependency array ensures this runs only once on component mount
+
   const updateOrder = (id, operation) => {
     let updatedOrder = { ...orderItems };
     if (operation === "+") {
-      if (id in updatedOrder) {
-        updatedOrder[id] = updatedOrder[id] + 1;
-      } else {
-        updatedOrder[id] = 1;
-      }
+      updatedOrder[id] = (updatedOrder[id] || 0) + 1;
     } else {
-      if (id in updatedOrder) {
-        updatedOrder[id] = updatedOrder[id] === 0 ? 0 : updatedOrder[id] - 1;
-      }
+      updatedOrder[id] = Math.max((updatedOrder[id] || 0) - 1, 0);
     }
     setOrderItems(updatedOrder);
-    setCartItems(updatedOrder)
+    setCartItems(updatedOrder);
   };
+
+  const categories = [
+    { title: "Build your own burger", items: menuItems.burgerItems },
+    { title: "Sandwiches", items: menuItems.sandwichItems },
+    { title: "Drink Items", items: menuItems.drinkItems },
+    { title: "Sides", items: menuItems.sideItems },
+  ];
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <div className="menu-container">
-        <h2>Build your own burger</h2>
-        {(menuItems?.burgerItems || []).map((item, index) => (
-          <div key={index} className="menu-item menu-items">
-            <h2>{item.name}</h2>
-            <img
-              src={item.imgurl}
-              alt={`${item.name} served on a plate`}
-              width="180"
-              height="auto"
-            />
-            <p>{item.description}</p>
-            <p>Category: {item.category}</p>
-            <p>Price: ${item.price.toFixed(2)}</p>
-            <div className="desc">{item.desc}</div>
-            <div className="menu-item-controls">
-              <button
-                className="update_order_subtract"
-                onClick={() => updateOrder(item.id, "-")}
-              >
-                -
-              </button>
-              <span className="item-quantity">
-                {orderItems[item.id] || "0"}
-              </span>
-              <button
-                className="update_order_add"
-                onClick={() => updateOrder(item.id, "+")}
-              >
-                +
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="menu-container">
-        <h2>Sandwiches</h2>
-        {(menuItems?.sandwichItems || []).map((item, index) => (
-          <div key={index} className="menu-item menu-items">
-            <h2>{item.name}</h2>
-            <img
-              src={item.imgurl}
-              alt={`${item.name} served on a plate`}
-              width="180"
-              height="auto"
-            />
-            <p>{item.description}</p>
-            <p>Category: {item.category}</p>
-            <p>Price: ${item.price.toFixed(2)}</p>
-            <div className="desc">{item.desc}</div>
-            <div className="menu-item-controls">
-              <button
-                className="update_order_subtract"
-                onClick={() => updateOrder(item.id, "-")}
-              >
-                -
-              </button>
-              <span className="item-quantity">
-                {orderItems[item.id] || "0"}
-              </span>
-              <button
-                className="update_order_add"
-                onClick={() => updateOrder(item.id, "+")}
-              >
-                +
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="menu-container">
-        <h2>Drink Items</h2>
-        {(menuItems?.drinkItems || []).map((item, index) => (
-          <div key={index} className="menu-item menu-items">
-            <h2>{item.name}</h2>
-            <img
-              src={item.imgurl}
-              alt={`${item.name} served on a plate`}
-              width="180"
-              height="auto"
-            />
-            <p>{item.description}</p>
-            <p>Category: {item.category}</p>
-            <p>Price: ${item.price.toFixed(2)}</p>
-            <div className="desc">{item.desc}</div>
-            <div className="menu-item-controls">
-              <button
-                className="update_order_subtract"
-                onClick={() => updateOrder(item.id, "-")}
-              >
-                -
-              </button>
-              <span className="item-quantity">
-                {orderItems[item.id] || "0"}
-              </span>
-              <button
-                className="update_order_add"
-                onClick={() => updateOrder(item.id, "+")}
-              >
-                +
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="menu-container">
-        <h2>Sides</h2>
-        {(menuItems?.sideItems || []).map((item, index) => (
-          <div key={index} className="menu-item menu-items">
-            <h2>{item.name}</h2>
-            <img
-              src={item.imgurl}
-              alt={`${item.name} served on a plate`}
-              width="180"
-              height="auto"
-            />
-            <p>{item.description}</p>
-            <p>Category: {item.category}</p>
-            <p>Price: ${item.price.toFixed(2)}</p>
-            <div className="desc">{item.desc}</div>
-            <div className="menu-item-controls">
-              <button
-                className="update_order_subtract"
-                onClick={() => updateOrder(item.id, "-")}
-              >
-                -
-              </button>
-              <span className="item-quantity">
-                {orderItems[item.id] || "0"}
-              </span>
-              <button
-                className="update_order_add"
-                onClick={() => updateOrder(item.id, "+")}
-              >
-                +
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
+      {categories.map((category, index) => (
+        <MenuCategory
+          key={index}
+          title={category.title}
+          items={category.items || []}
+          updateOrder={updateOrder}
+          orderItems={orderItems}
+        />
+      ))}
     </div>
   );
 };
