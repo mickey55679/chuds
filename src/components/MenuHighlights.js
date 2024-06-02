f64cbcb7cc28d398033be5e8bdde9f60ebbff387
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-02 15:06:09 -0500

    wip

--- src/components/MenuHighlights.js
+++ src/components/MenuHighlights.js
@@ -6,22 +6,34 @@ const MenuHighlights = () => {
       <h3 className="menu-highlights-title">Menu Highlights</h3>
       <div className="menu-highlights-container">
         <div className="menu-highlights-item">
-          <img src="family-style-meals.jpg" alt="Family Style Meals" />
+          <img
+            src="https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg"
+            alt="Family Style Meals"
+          />
           <h3>Family Style Meals</h3>
           <button>Order Now</button>
         </div>
         <div className="menu-highlights-item">
-          <img src="appetizers.jpg" alt="Appetizers" />
+          <img
+            src="https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg"
+            alt="Appetizers"
+          />
           <h3>Appetizers</h3>
           <button>Order Now</button>
         </div>
         <div className="menu-highlights-item">
-          <img src="classic-entrees.jpg" alt="Classic Entrees" />
+          <img
+            src="https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg"
+            alt="Classic Entrees"
+          />
           <h3>Classic Entrées</h3>
           <button>Order Now</button>
         </div>
         <div className="menu-highlights-item">
-          <img src="create-your-own-pasta.jpg" alt="Create Your Own Pasta" />
+          <img
+            src="https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg"
+            alt="Create Your Own Pasta"
+          />
           <h3>Create Your Own Pasta</h3>
           <button>Order Now</button>
         </div>
