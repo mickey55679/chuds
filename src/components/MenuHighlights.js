d70c05d0e4ec2a72a8e6ac738477d32c48a7bba0
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-02 14:30:25 -0500

    wip

--- src/components/MenuHighlights.js
+++ src/components/MenuHighlights.js
@@ -1,27 +1,26 @@
 import React from "react";
 
-
 const MenuHighlights = () => {
   return (
     <div className="menu-highlights-section">
-      <h3 className="menu-title">Menu Highlights</h3>
+      <h3 className="menu-highlights-title">Menu Highlights</h3>
       <div className="menu-highlights-container">
-        <div className="menu-item">
+        <div className="menu-highlights-item">
           <img src="family-style-meals.jpg" alt="Family Style Meals" />
           <h3>Family Style Meals</h3>
           <button>Order Now</button>
         </div>
-        <div className="menu-item">
+        <div className="menu-highlights-item">
           <img src="appetizers.jpg" alt="Appetizers" />
           <h3>Appetizers</h3>
           <button>Order Now</button>
         </div>
-        <div className="menu-item">
+        <div className="menu-highlights-item">
           <img src="classic-entrees.jpg" alt="Classic Entrees" />
           <h3>Classic Entrées</h3>
           <button>Order Now</button>
         </div>
-        <div className="menu-item">
+        <div className="menu-highlights-item">
           <img src="create-your-own-pasta.jpg" alt="Create Your Own Pasta" />
           <h3>Create Your Own Pasta</h3>
           <button>Order Now</button>
