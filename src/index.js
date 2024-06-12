2648e2be741f6ef2d74ad63c552d06c9d454680f
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-12 16:11:43 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { AuthProvider} from "./auth/AuthContext";
+
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <AuthProvider>
+
     <App />
-  </AuthProvider>
+ 
 );
