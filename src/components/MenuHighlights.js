a6bc10df00cecffeb2bc06e47c60a2d5aaa3ce33
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-13 13:56:37 -0500

    updated images for visualization in menuHighlights

--- src/components/MenuHighlights.js
+++ src/components/MenuHighlights.js
@@ -7,7 +7,7 @@ const MenuHighlights = () => {
       <div className="menu-highlights-container">
         <div className="menu-highlights-item">
           <img
-            src="https://scontent.ffsd2-1.fna.fbcdn.net/v/t39.30808-6/447631074_437655552382939_3159214166711699973_n.jpg?stp=cp6_dst-jpg&_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oZrfJVatwsMQ7kNvgH6xkqV&_nc_ht=scontent.ffsd2-1.fna&oh=00_AYCtgOiKROwiQp5EP3wpey40bgEtOJ-GceET7D7TuriRyA&oe=6666D0A1"
+            src="https://cdn.pixabay.com/photo/2021/01/19/08/47/sandwich-5930496_1280.jpg"
             alt="Caramelized Mushroom Onion Philly "
           />
           <h3>Carmelized Mushroom Onion Philly</h3>
@@ -15,7 +15,7 @@ const MenuHighlights = () => {
         </div>
         <div className="menu-highlights-item">
           <img
-            src="https://scontent.ffsd2-1.fna.fbcdn.net/v/t39.30808-6/447974607_438272138987947_8784784681373250269_n.jpg?stp=cp6_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=QCQOj8uKY8sQ7kNvgHzPYDh&_nc_ht=scontent.ffsd2-1.fna&oh=00_AYBpN6eX2GlMOH1FWMGeQCMolDH-O50087KdDDk9Y1cvRg&oe=666810A4"
+            src="https://cdn.pixabay.com/photo/2017/07/08/17/13/steak-2484999_1280.jpg"
             alt="Appetizers"
           />
           <h3>Appetizers</h3>
@@ -23,7 +23,7 @@ const MenuHighlights = () => {
         </div>
         <div className="menu-highlights-item">
           <img
-            src="https://scontent.ffsd2-1.fna.fbcdn.net/v/t39.30808-6/447511009_435864252562069_566068263162962865_n.jpg?stp=cp6_dst-jpg&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=funprnVbnlwQ7kNvgHogxr2&_nc_ht=scontent.ffsd2-1.fna&oh=00_AYDKaSNreHkIVkBhelA5B87oaD9yBj1TBuwYv9TMz0g1jA&oe=66682E86"
+            src="https://cdn.pixabay.com/photo/2018/09/21/18/25/fillet-3693670_1280.jpg"
             alt="Classic Entrees"
           />
           <h3>Classic Entrées</h3>
@@ -31,7 +31,7 @@ const MenuHighlights = () => {
         </div>
         <div className="menu-highlights-item">
           <img
-            src="https://scontent.ffsd2-1.fna.fbcdn.net/v/t39.30808-6/441525521_434784872670007_6474962904387491074_n.jpg?stp=cp6_dst-jpg&_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ZH_P-5F5abwQ7kNvgHyQHCn&_nc_ht=scontent.ffsd2-1.fna&oh=00_AYA11IRrzxy6ISTkxVwy3ecEkV0CZ2MBk0HWYt8rKuAifg&oe=666817C4"
+            src="https://cdn.pixabay.com/photo/2016/12/11/22/41/lasagna-1900529_1280.jpg"
             alt="Create Your Own Pasta"
           />
           <h3>Create Your Own Pasta</h3>
