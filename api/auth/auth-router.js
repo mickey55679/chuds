4143f945794f02a254cc22be1ac58cca4e969e42
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-13 17:07:21 -0500

    wip notes

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,15 +1,15 @@
 const express = require('express')
 
 const router = express.Router()
-// register is a post request because we need to provide credentials 
+// post because of payload
 router.post('/register', async (req, res, next) => {
  res.json({message: 'register is working'})
 })
-// login requires credentials as well so it needs to be a post as well
+// post because of payload
 router.post("/login", async (req, res, next) => {
 res.json({message: 'login working'})
 });
-// get request for logout because no payload is required
+
 router.get("/logout", async (req, res, next) => {
 res.json({ message: "logout working" });
 });
