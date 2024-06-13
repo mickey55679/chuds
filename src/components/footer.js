8490080b430fb644da2af3960da5209aa739e336
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-13 14:32:09 -0500

    wip footer adjustments

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,42 +1,68 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faPhone, faCar } from "@fortawesome/free-solid-svg-icons"; // Import the car icon
+import {
+  faFacebook,
+  faTwitter,
+  faGoogle,
+  faFlickr,
+} from "@fortawesome/free-brands-svg-icons";
+import { faPhone, faCar, faRss } from "@fortawesome/free-solid-svg-icons";
+
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div
-        className="info-footer"
-        style={{ display: "flex", justifyContent: "space-around" }}
-      >
-        {/* Add any other information here */}
+      <div className="footer-top">
       </div>
-      <div
-        className="social-media"
-        style={{ display: "flex", justifyContent: "space-around" }}
-      >
-        <a
-          href="https://www.facebook.com/people/CHUDS-Pub-and-Grub/100084154931963/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faFacebook} size="2x" />
-        </a>
-        <a
-          href="tel:+17124772469" // Format the phone number for use in a link
-          className="phone-icon" // Optional, for styling
-        >
-          <FontAwesomeIcon icon={faPhone} size="2x" />
-        </a>
-        <a
-          href="https://www.google.com/maps/dir/?api=1&destination=403+Holder+St,+Larchwood,+IA+51241"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="car-icon" // Optional, for styling
-        >
-          <FontAwesomeIcon icon={faCar} size="2x" />
-        </a>
+      <div className="footer-bottom">
+        <div className="social-media">
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+          <a
+            href="https://www.rss.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faRss} size="2x" />
+          </a>
+          <a
+            href="https://www.google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGoogle} size="2x" />
+          </a>
+          <a
+            href="https://www.flickr.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFlickr} size="2x" />
+          </a>
+          <a href="tel:+17124772469">
+            <FontAwesomeIcon icon={faPhone} size="2x" />
+          </a>
+          <a
+            href="https://www.google.com/maps/dir/?api=1&destination=403+Holder+St,+Larchwood,+IA+51241"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faCar} size="2x" />
+          </a>
+        </div>
+        <p>© Copyright. All rights reserved.</p>
       </div>
     </footer>
   );
