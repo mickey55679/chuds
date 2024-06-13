57dbc16beae396dd686f9ec8aac8a3dafcd39dd7
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-13 14:56:56 -0500

    added new seeds

--- database/migrations/20240609140657_create_users_table.js
+++ database/migrations/20240609140657_create_users_table.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("users", function (table) {
-    table.increments("id").primary();
-    table.string("email").notNullable().unique();
-    table.string("password").notNullable();
+  return knex.schema.createTable("users", (tbl) => {
+    tbl.increments();
+    tbl.string("username", 128).notNullable().unique().index();
+    tbl.string("password", 256).notNullable();
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("users");
+  return knex.schema.dropTableIfExists("users");
 };
