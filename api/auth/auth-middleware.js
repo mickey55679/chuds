b98357d037717521cf4939174e28edead759afda
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-14 18:47:00 -0500

    wip admin

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,9 @@
 async function restricted(req, res, next) {
-console.log('restricted is working')
+if(req.session.user) {
+    next()
+} else {
+    next({message: 'You shall not pass!'})
+}
 }
 async function validatePayload(req, res, next){
     console.log('validate payload is working')
