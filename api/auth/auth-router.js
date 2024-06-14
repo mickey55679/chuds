6986c865f33ca2a10b837187f70aea3a6b94d0e0
Michaiah Bos <michaiahbos@yahoo.com>
2024-06-14 17:19:40 -0500

    /register endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -10,9 +10,11 @@ const {username, password} = req.body;
 const hash = bcrypt.hashSync(password, 8)
 const newUser = {username, password: hash}
 const result = await User.add(newUser)
+res.status(201).json({
+    message:   `nice to have you ${result.username}`
+})
 } catch (err) {
-    
-
+next(err)
 }
 
 
