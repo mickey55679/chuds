3b4d01eb19b8ac2099068670e68f89a961e6463d
Michaiah Bos <michaiahbos@yahoo.com>
2024-07-17 17:17:29 -0500

    got rid of extra css

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { chudsphoto } from "./images/index";
+// import { chudsphoto } from "./images/index";
 import MenuHighlights from "./MenuHighlights";
+// import myLogo from './images/chudsgroup-removebg-preview.png'
 
 const Home = (props) => {
   return (
     <>
       <div className="home-page">
         <div className="image-container">
-          <img src={chudsphoto} alt="Home" className="background-image" />
+          <img
+            src="https://cdn.pixabay.com/photo/2017/04/22/22/52/beer-2252586_1280.jpg"
+            alt="Home"
+            className="background-image"
+          />
           <div className="overlay-text">
+            {/* <img src={myLogo} alt="logo" className="my-logo-home" /> */}
             <p>
               CHUDs Pub and Grub is a family friendly restaurant that offers
               delicious homemade food and a full bar!
