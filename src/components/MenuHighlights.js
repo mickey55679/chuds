aae50b29975a697130eb37082de5f4a125c0bec9
Michaiah Bos <michaiahbos@yahoo.com>
2024-08-05 19:26:04 -0500

    adjusted menu and menuHighlights

--- src/components/MenuHighlights.js
+++ src/components/MenuHighlights.js
@@ -8,34 +8,23 @@ const MenuHighlights = () => {
       <h2 className="menu-highlights-title">Menu Highlights</h2>
       <div className="menu-highlights-container">
         <div className="menu-highlights-item">
-          <img
-            src={wrap}
-            alt="Caramelized Mushroom Onion Philly "
-          />
           <h3>Chicken wrap</h3>
+          <img src={wrap} alt="Caramelized Mushroom Onion Philly " />
+
           <button>Order Now</button>
         </div>
         <div className="menu-highlights-item">
-          <img
-            src={notchos}
-            alt="Appetizers"
-          />
+          <img src={notchos} alt="Appetizers" />
           <h3>Appetizers</h3>
           <button>Order Now</button>
         </div>
         <div className="menu-highlights-item">
-          <img
-            src={salad}
-            alt="Classic Entrees"
-          />
+          <img src={salad} alt="Classic Entrees" />
           <h3>Classic Entrées</h3>
           <button>Order Now</button>
         </div>
         <div className="menu-highlights-item">
-          <img
-            src={sandwich}
-            alt="Create Your Own Pasta"
-          />
+          <img src={sandwich} alt="Create Your Own Pasta" />
           <h3>Create Your Own Pasta</h3>
           <button>Order Now</button>
         </div>
