c79d60442a11f0337e1d48818733b8aae7bff800
Michaiah Bos <michaiahbos@yahoo.com>
2024-09-16 13:57:05 -0500

    updates to user-router

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,16 +1,15 @@
 const router = require("express").Router();
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
+
 const Users = require("./users-model");
 const {restricted} = require('../auth/auth-middleware')
 
 
 router.get("/", restricted, (req, res, next) => {
   Users.find()
-    .then((users) => {
-      res.json(users);
+    .then(users => {
+      res.status(200).json(users);
     })
-    .catch(next); // our custom err handling middleware will trap this
+    .catch(next); 
 });
 
 module.exports = router;
