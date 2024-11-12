8f2f6b4fd02f9601619649cbbc19e04e0ec12f12
Michaiah Bos <michaiahbos@yahoo.com>
2024-11-11 20:04:24 -0600

    got rid of second box

--- src/components/home.js
+++ src/components/home.js
@@ -53,25 +53,12 @@ const Home = ({ handleClick }) => {
       <div className="flex flex-col md:flex-row items-center md:space-x-8 space-y-8 md:space-y-0 p-4 min-h-max bg-stone-300  border-2 border-black">
         {/* First Box */}
         <div className="flex flex-col items-center space-y-8 p-8 border-2 rounded-md border-black  bg-slate-100">
-          <h1 className="text-xl">Delicious Comfort Food</h1>
-          <p className="text-center p-5 font-serif text-lg">
-            Chuds Pub and Grub is a family-friendly restaurant offering
-            delicious homemade food and a full bar!
-          </p>
-          <Link to="/menu">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Order Now
-            </button>
-          </Link>
-        </div>
-
-        {/* Second Box */}
-        <div className="flex flex-col items-center space-y-8 p-8 border-2 rounded-md border-black  bg-slate-100">
           <img
             src={barTap}
             alt="Description of barTap image"
             className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-auto object-cover rounded-md"
           />
+          <h1 className="text-xl">Delicious Comfort Food</h1>
           <p className="text-center">
             Lorem ipsum odor amet, consectetuer adipiscing elit. Eleifend platea
             et aenean porttitor nullam. Vestibulum sed senectus lobortis
@@ -81,6 +68,11 @@ const Home = ({ handleClick }) => {
             sollicitudin porttitor quisque. Tempor ipsum potenti et nullam
             consectetur sit sagittis.
           </p>
+          <Link to="/menu">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Order Now
+            </button>
+          </Link>
         </div>
       </div>
 
