26a92a4b7458ad448524b567e05d23f4d7258867
Michaiah Bos <michaiahbos@yahoo.com>
2025-01-09 17:34:41 -0600

    fixed space issue

--- src/components/About.js
+++ src/components/About.js
@@ -12,16 +12,21 @@ const About = () => {
         backgroundPosition: "center",
       }}
     >
-      <div className="max-w-3xl text-center p-6 bg-white rounded-lg shadow-md">
+      {/* About Us Section */}
+      <div className="max-w-3xl text-center p-6 bg-white rounded-lg shadow-md mb-12">
         <h1 className="text-4xl font-bold text-gray-800 mb-6">About Us</h1>
-        <p className="text-lg text-gray-600 mb-4">
-          CHUDS is a veteran-owned and operated restaurant with homemade food.
-          We are known for our delicious menu selection and great service. We
-          offer a full bar, flatbreads, hand-pattied burgers, sandwiches, and a
-          good variety of other options. If you are looking for a short drive
-          out of the city, our place is what you are looking for.
-        </p>
+        <div>
+          <p className="text-lg text-gray-600">
+            CHUDS is a veteran-owned and operated restaurant with homemade food.
+            We are known for our delicious menu selection and great service. We
+            offer a full bar, flatbreads, hand-pattied burgers, sandwiches, and
+            a good variety of other options. If you are looking for a short
+            drive out of the city, our place is what you are looking for.
+          </p>
+        </div>
       </div>
+
+      {/* Carousel Section */}
       <Carousel />
     </div>
   );
